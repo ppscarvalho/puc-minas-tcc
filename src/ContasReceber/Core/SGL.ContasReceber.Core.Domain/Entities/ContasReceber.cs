cba40a7616603ed8c0f54f1f1aa3391a921af0be
Pedro Carvalho <ppscarvalho@gmail.com>
2023-06-29 01:09:12 -0300

    Implementa classe de dominio contas a receber

---
+++ src/ContasReceber/Core/SGL.ContasReceber.Core.Domain/Entities/ContasReceber.cs
@@ -0,0 +1,31 @@
+﻿using SGL.ContasReceber.Core.Domain.Enuns;
+using SGL.Resource.Domain;
+using SGL.Resource.Interfaces;
+
+namespace SGL.ContasReceber.Core.Domain.Entities
+{
+    public class ContasReceber : Entity, IAggregateRoot
+    {
+        public Guid ClienteId { get; private set; }
+        public string? Descricao { get; private set; }
+        public DateTime DataVencimento { get; private set; }
+        public decimal Valor { get; private set; }
+        public ESituacao Situacao { get; private set; }
+
+        public ContasReceber() { }
+
+        public ContasReceber(Guid clienteId, string? descricao, DateTime dataVencimento, decimal valor) : this()
+        {
+            ClienteId = clienteId;
+            Descricao = descricao;
+            DataVencimento = dataVencimento;
+            Valor = valor;
+            Situacao = ESituacao.A_Receber;
+        }
+
+        public void AlterarSituacaoParaPago()
+        {
+            Situacao = ESituacao.Pago;
+        }
+    }
+}
