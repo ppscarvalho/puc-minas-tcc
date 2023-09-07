39cf154480961dc37aa6cfcb8c2d57b3c8c5d14a
Pedro Carvalho <ppscarvalho@gmail.com>
2023-09-07 08:55:24 -0300

    Refatorando ContasPagarController

--- src/Web/SGL.WebUI/Controllers/ContasPagarController.cs
+++ src/Web/SGL.WebUI/Controllers/ContasPagarController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SGL.Integrations.Interfaces;
+using SGL.Integrations.ViewModels;
 
 namespace SGL.WebUI.Controllers
 {
-    public class ContasPagarController : Controller
+    public class ContasPagarController : BaseController
     {
         private readonly IContasPagarService _contasPagarService;
 
@@ -12,18 +14,76 @@ namespace SGL.WebUI.Controllers
             _contasPagarService = contasPagarService;
         }
 
-        // GET: CustomerController
+        // GET: ContasPagarController
+        [Authorize]
         public async Task<IActionResult> Index()
         {
-            var contasPagar = await _contasPagarService.ObterTodasContasPagar();
+            var token = await GetToken();
+            var ContasPagar = await _contasPagarService.ObterTodosContasPagars(token);
+            return View(ContasPagar);
+        }
+
+        // GET: ContasPagarController/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            var contasPagar = new ContasPagarViewModel();
             return View(contasPagar);
         }
 
-        // GET: CustomerController/Details/5
+        // POST: ContasPagarController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create(ContasPagarViewModel contasPagarViewModel)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View();
+
+                var token = await GetToken();
+                var result = await _contasPagarService.Adicionar(contasPagarViewModel, token);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContasPagarController/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var token = await GetToken();
+            var contasPagar = await _contasPagarService.ObterContasPagarPorId(id, token);
+            return View(contasPagar);
+        }
+
+        // POST: ContasPagarController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Edit(ContasPagarViewModel ContasPagarViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var token = await GetToken();
+            var result = await _contasPagarService.Atualizar(ContasPagarViewModel, token);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ContasPagarController/Details/5
+        [Authorize]
         public async Task<IActionResult> Details(Guid id)
         {
-            var contasPagar = await _contasPagarService.ObterContasPagarPorId(id);
-            return View(contasPagar);
+            var token = await GetToken();
+            var ContasPagar = await _contasPagarService.ObterContasPagarPorId(id, token);
+            return View(ContasPagar);
         }
     }
 }
